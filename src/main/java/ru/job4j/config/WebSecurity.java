689a684c834ed2e7ba338484396caceb34f1afac
aseldom <a.seldom@gmail.com>
2024-02-22 07:41:51 +0600

    small fix

--- src/main/java/ru/job4j/config/WebSecurity.java
+++ src/main/java/ru/job4j/config/WebSecurity.java
@@ -31,7 +31,6 @@ public class WebSecurity extends WebSecurityConfigurerAdapter  {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().authorizeRequests()
                 .antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
-//                .antMatchers("/site/convert").permitAll()
                 .antMatchers("/site/redirect/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
